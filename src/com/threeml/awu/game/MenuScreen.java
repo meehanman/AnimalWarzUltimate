03468d7d686b0666e4bfc365eeadc531ed6e51ec
40130351 <Maryjanem@users.noreply.github.com>
2015-02-06 14:19:12 +0000

    MJ - added music to main menu

--- src/com/threeml/awu/game/MenuScreen.java
+++ src/com/threeml/awu/game/MenuScreen.java
@@ -1,10 +1,17 @@
 package com.threeml.awu.game;
 
+import java.io.IOException;
 import java.util.List;
 
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Rect;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.util.Log;
 
 import com.threeml.awu.Game;
 import com.threeml.awu.engine.AssetStore;
@@ -21,7 +28,20 @@ import com.threeml.awu.world.GameScreen;
  * @version 1.0
  */
 public class MenuScreen extends GameScreen {
-
+	
+	/**
+	 * Define the variables for sound effects
+	 */
+	private SoundPool mSoundPool;
+	private final int mMaxChannels = 20;
+	private int mButtonDingId = -1;
+	
+	private MediaPlayer mMediaPlayer;
+	private boolean mMediaAvailable = false;
+	
+	private float volume = 1;
+	private float speed = 0.05f;
+	
 	/**
 	 * Define the trigger touch region for playing the 'games'
 	 */
@@ -43,7 +63,32 @@ public class MenuScreen extends GameScreen {
 		assetManager.loadAndAddBitmap("MainMenuBackground", "img/background/lostKingdom.png");
 		//Load in button images
 		assetManager.loadAndAddBitmap("NewGameButton", "img/MainMenu/newGameButton.png");
-
+		
+		mSoundPool = new SoundPool(mMaxChannels, AudioManager.STREAM_MUSIC, 0);
+		AssetManager am = game.getActivity().getAssets();
+		try {
+			
+			AssetFileDescriptor assetDescriptor = am.openFd("sfx/click2.ogg");
+		} catch (IOException e) {
+			Log.v("Error", "Couldn't load sfx");
+		}
+		
+		game.getActivity().setVolumeControlStream(AudioManager.STREAM_MUSIC);
+		
+		mMediaPlayer = new MediaPlayer();
+		try {
+			AssetFileDescriptor musicDescriptior = am.openFd("music/Video_Dungeon_Boss.mp3");
+			mMediaPlayer.setDataSource(musicDescriptior.getFileDescriptor(),
+					musicDescriptior.getStartOffset(),
+					musicDescriptior.getLength());
+			mMediaPlayer.setLooping(true);
+			mMediaPlayer.prepare();
+			
+			mMediaAvailable = true;
+		} catch (IOException e) {
+			Log.v("Error", "Couldn't load music");
+		}
+		
 		/*//Default GAGE CODE
 		// Define the rects what will be used to 'hold' the images
 		int spacingX = game.getScreenWidth() / 6;
@@ -75,6 +120,11 @@ public class MenuScreen extends GameScreen {
 			TouchEvent touchEvent = touchEvents.get(0);
 
 			if (mPlayGameBound.contains((int) touchEvent.x,	(int) touchEvent.y)) {
+				if(mButtonDingId != -1){
+					mSoundPool.play(mButtonDingId, 10.0f, 10.0f, 1, 0, 1.0f);
+				}
+				mSoundPool.release();
+				mMediaPlayer.release();
 				// If the play game area has been touched then swap screens
 				mGame.getScreenManager().removeScreen(this.getName());
 				SinglePlayerGameScreen singlePlayerGameScreen = new SinglePlayerGameScreen(mGame);
@@ -114,5 +164,46 @@ public class MenuScreen extends GameScreen {
 		graphics2D.clear(Color.WHITE);
 		graphics2D.drawBitmap(Background, null, mBackgroundBound, null);
 		graphics2D.drawBitmap(playGame, null, mPlayGameBound, null);
+	}
+	@Override
+	public void resume() {
+		super.resume();
+		
+		if(mMediaAvailable){
+			this.FadeIn(3);
+			mMediaPlayer.start();
+		}
+	}
+	
+	
+	@Override
+	public void pause() {
+		super.pause();
+		
+		if(mMediaAvailable) {
+			mMediaPlayer.pause();
+			
+			if(mGame.getActivity().isFinishing()){
+				mMediaPlayer.stop();
+				mMediaPlayer.release();
+				mSoundPool.release();
+			}
+		}
+	}
+	
+	
+	//this stuff needs work.
+	//src: http://stackoverflow.com/questions/6884590/android-how-to-create-fade-in-fade-out-sound-effects-for-any-music-file-that-my
+	private void FadeOut(float deltaTime)
+	{
+	    mMediaPlayer.setVolume(volume, volume);
+	    volume -= speed* deltaTime;
+
+	}
+	private void FadeIn(float deltaTime)
+	{
+		mMediaPlayer.setVolume(volume, volume);
+	    volume += speed* deltaTime;
+
 	}
 }
