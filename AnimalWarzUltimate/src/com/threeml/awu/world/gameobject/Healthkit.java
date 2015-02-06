e695be414cc762ad0f7e71d0152fe6e01395d318
40130351 <40130351@755a74bb-203b-7d4a-9881-852534eabff6>
2015-02-06 12:09:29 +0000

    MJ - more testing svn with small architecture changes... this is the last one! unless it's not.

---
+++ AnimalWarzUltimate/src/com/threeml/awu/world/gameobject/Healthkit.java
@@ -0,0 +1,18 @@
+package com.threeml.awu.world.gameobject;
+
+import android.graphics.Bitmap;
+
+import com.threeml.awu.world.GameScreen;
+
+public class Healthkit extends Item{
+
+	public Healthkit(float startX, float startY, Bitmap bitmap, GameScreen gameScreen) {
+		super(startX, startY, 20.0f, 20.0f, bitmap, gameScreen);
+		
+	}
+
+	public int getHealthValue() {
+		return 50;
+	}
+
+}
