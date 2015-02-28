ffa997cce16b1cd64baa980673a136fdac1b6697
40130351 <40130351@755a74bb-203b-7d4a-9881-852534eabff6>
2015-02-28 22:48:15 +0000

    MJ - used framehandler class to get bitmapfont working (need to tidy up laterz)

--- AnimalWarzUltimate/src/com/threeml/awu/util/BitmapFont.java
+++ AnimalWarzUltimate/src/com/threeml/awu/util/BitmapFont.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.util.Log;
 
 import com.threeml.awu.engine.ElapsedTime;
 import com.threeml.awu.engine.graphics.IGraphics2D;
+import com.threeml.awu.world.FrameHandler;
 import com.threeml.awu.world.GameObject;
 import com.threeml.awu.world.GameScreen;
 import com.threeml.awu.world.LayerViewport;
@@ -15,132 +17,182 @@ import com.threeml.awu.world.ScreenViewport;
 import com.threeml.awu.world.Sprite;
 
 /**
- * BitmapFont class Draw Text on Screen 
+ * BitmapFont class Draw Text on Screen
  * 
- * @author Dean
- *
- *
+ * @author Dean & Mary-Jane
+ * 
+ * 
  */
 
 public class BitmapFont extends Sprite {
-	
+
+	FrameHandler mFrameHandler;
+	private Bitmap mTextImage;
+	private final int mTextCellwidth = 12;
+	private final int mTextCellheight = 18;
 	List<Bitmap> BitmapList = new ArrayList<Bitmap>();
-	private int textCellwidth = 12;
-	private int textCellheight = 16;
-	
-	public BitmapFont(float x, float y, GameScreen gameScreen, String str) {
-		super(x, y, 50.0f, 50.0f, gameScreen.getGame()
-				.getAssetManager().getBitmap("Font"), gameScreen);
+	private int mFontSize;
+	private String mText;
+	private static Bitmap mFont;
 
-		//Create a default string
-		updateString(str);
+	public BitmapFont(float x, float y, GameScreen gameScreen, String str,
+			int fontSize) {
+		// super(x, y, (str.toCharArray().length * mTextCellwidth),
+		// mTextCellheight, mFont, gameScreen);
+		super(x, y, 200, 20, mFont, gameScreen);
+		try {
+
+			mFont = gameScreen.getGame().getAssetManager().getBitmap("Font");
+			mFontSize = fontSize;
+			// Create a default string
+			mFrameHandler = new FrameHandler(mFont, 2, 53);
+			mText = str;
+			mTextImage = Bitmap.createBitmap(mText.length() * mTextCellwidth,
+					mTextCellheight, Bitmap.Config.ARGB_8888);
+			mTextImage = getTextAsImage();
+		} catch (Exception e) {
+			// Log.e("Text Error", "BitmapFont constructor error : " + e);
+		}
 	}
-	
-	//Returns an array of bitmaps to be drawn from the string
-	public void updateString(String str){
-		char[] letterArray = str.toCharArray();
-	
-		for(int i=0;i<letterArray.length;i++){
-			//Returns a bitmap of the letter
-			BitmapList.add(drawLetter(letterArray[i]));	
+
+	private Bitmap getTextAsImage() {
+		try {
+			try {
+
+				updateString();
+			} catch (Exception e) {
+				Log.v("Text Test", "Image error : " + e);
+			}
+			Canvas canvas = new Canvas(mTextImage);
+			int count = 0;
+			for (Bitmap b : BitmapList) {
+				canvas.drawBitmap(b, (count * mTextCellwidth), 0, null);
+				count++;
+			}
+			// Log.v("Text Test", "After | Image size : " +
+			// mTextImage.getWidth() + ", " + mTextImage.getHeight());
+		} catch (Exception e) {
+			// Log.e("Text Error", "BitmapFont getTextAsImage error : " + e);
+		}
+		return mTextImage;
+	}
+
+	// Returns an array of bitmaps to be drawn from the string
+	public void updateString() {
+		try {
+			// Log.v("Text Test", mText);
+
+			for (char c : mText.toCharArray()) {
+				// Returns a bitmap of the letter
+				BitmapList.add(getLetterBitmap(c));
+
+			}
+		} catch (Exception e) {
+			// Log.e("Text Error", "BitmapFont updateString error : " + e);
 		}
 
 	}
-	//Return a bitmap of the char
-	private Bitmap drawLetter(char c){
 
-		int locationx = 0,locationy = 0;
-		char[] special = {'.', ':',',',';','\'','"','(','!','?',')','+','-','*','/','='}; //Special Chars in order on Bitmap
-		
-		/**Pixel locations of the different sets in the image...
-		*look at the image to understand
-		*Lowercase(0,0),(312,16)
-		*UpperCase(324,0),(636,16)
-		*Num(0,19),(120,35)
-		*Special (120,19),(300,35). : , ; ' " ( ! ? ) + - * / =
-		*/
-		//Logic to get location of letter using ASCII location of chars
-		if(c >= 'a' && c <= 'z'){ //97 - 122
-			locationx = ((int)c-97)*textCellwidth;
-			locationy = 0; 
-		}else if(c >= 'A' && c <= 'Z'){ //65 - 90
-			locationx = 324 + (((int)c-65)*textCellwidth);
-			locationy = 0; 
-		}else if((int)c >=0 && (int)c <=9){ //48-57
-			locationx = ((int)c-48)*textCellwidth;;
-			locationy = 19; 
-		}else{
-			boolean found = false;
-			for(int i = 0;i<special.length;i++){
-				if(special[i]==c){
-					found = true;
-					locationx = 120+(i)*textCellwidth;
-					locationy = 19; 
-					break;
+	private Bitmap getLetterBitmap(char ch) {
+		try {
+			int r = 0, c = 0;
+
+			char[] special = { '.', ':', ',', ';', '\'', '"', '(', '!', '?',
+					')', '+', '-', '*', '/', '=' }; // Special Chars in order on
+													// Bitmap
+
+			/**
+			 * Pixel locations of the different sets in the image... look at the
+			 * image to understand Lowercase(0,0),(312,16)
+			 * UpperCase(324,0),(636,16) Num(0,19),(120,35) Special
+			 * (120,19),(300,35). : , ; ' " ( ! ? ) + - * / =
+			 */
+			// Logic to get location of letter using ASCII location of chars
+			if (ch >= 'a' && ch <= 'z') { // 97 - 122
+				c = (int) ch - 97;
+				r = 0;
+			} else if (ch >= 'A' && ch <= 'Z') { // 65 - 90
+				c = (int) ch - 65;
+				r = 0;
+			} else if (ch >= '0' && ch <= '9') { // 48-57
+				if(ch == '0'){
+					c = 9;
+				}else {
+					c = (int) ch - 49;
 				}
+				r = 1;
+			} else {
+				boolean found = false;
+				/*
+				 * for(int i = 0;i<special.length;i++){ if(special[i]==ch){
+				 * found = true; c = 120+(i)*textCellwidth; r = 1; break; } }
+				 */
+				if (!found) { // If not found, output space which is between z
+								// and A
+					c = 0;
+					r = 0;
+				}
+
 			}
-			if(!found){ //If not found, output space which is between z and A
-				locationx = 324 - textCellwidth;
-				locationy = 0; 
-			}
+			mFrameHandler.setFrame(r, c);
+			// Log.v("Image Test", "Char : " + ch + " |Col : " + c + " |Row : "
+			// + r);
+		} catch (Exception e) {
+			// Log.e("Text Error", "BitmapFont getLetterBitmap error : " + e);
 		}
-		
-		Log.v("bitmap?",mBitmap.getWidth()+"");
-		return Bitmap.createBitmap(mBitmap, locationx, locationy, textCellwidth, textCellheight);
+
+		return mFrameHandler.getFrameImage();
 	}
-	
+
 	/**
 	 ** Update method that will follow an object like player (for health etc)
-	 **	@author Dean 
+	 ** 
+	 * @author Dean
 	 **/
 	public void update(ElapsedTime elapsedTime, GameObject GameObj) {
-		
+
 		this.position = GameObj.position;
 		this.setY(GameObj.getBound().y + 5f);
 
 		super.update(elapsedTime);
-		
+
 	}
-	
+
 	/**
 	 * @author Dean
 	 * 
-	 * Override Draw method used to display text on screen from the custom
-	 * bitmaps created by drawLetter.  
+	 *         Override Draw method used to display text on screen from the
+	 *         custom bitmaps created by drawLetter.
 	 */
 	@Override
 	public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D,
 			LayerViewport layerViewport, ScreenViewport screenViewport) {
-		
-		if (GraphicsHelper.getSourceAndScreenRect(this, layerViewport,
-				screenViewport, drawSourceRect, drawScreenRect)) {
+		try {
+			if (GraphicsHelper.getSourceAndScreenRect(this, layerViewport,
+					screenViewport, drawSourceRect, drawScreenRect)) {
 
-			float scaleX = 
-					(float) drawScreenRect.width() 
+				float scaleX = (float) drawScreenRect.width()
 						/ (float) drawSourceRect.width();
-			float scaleY = 
-					(float) drawScreenRect.height() 
+				float scaleY = (float) drawScreenRect.height()
 						/ (float) drawSourceRect.height();
 
-			// Build an appropriate transformation matrix
-			drawMatrix.reset();
-			drawMatrix.postScale(scaleX, scaleY);
-			drawMatrix.postRotate(orientation, scaleX * mBitmap.getWidth()
-					/ 2.0f, scaleY * mBitmap.getHeight() / 2.0f);
-			drawMatrix.postTranslate(drawScreenRect.left, drawScreenRect.top);
-			
-			float BoundXPos = this.getBound().x;
-			// Draw the image
-			for(int i = 0; i<BitmapList.size();i++){
-				//Move the drawlocation over by cellwidth for each letter
-				this.setX(this.getBound().x += textCellwidth*i);
-				//Draw the bitmap on screen
-				graphics2D.drawBitmap(BitmapList.get(i), drawMatrix, null);
+				// Build an appropriate transformation matrix
+				drawMatrix.reset();
+				drawMatrix.postScale(scaleX, scaleY);
+				drawMatrix.postRotate(orientation, scaleX * mBitmap.getWidth()
+						/ 2.0f, scaleY * mBitmap.getHeight() / 2.0f);
+				drawMatrix.postTranslate(drawScreenRect.left,
+						drawScreenRect.top);
+
+				float BoundXPos = this.getBound().x;
+				// Draw the image
+				graphics2D.drawBitmap(getTextAsImage(), drawMatrix, null);
+
+				// Reset the draw location for the images
+				this.getBound().x = BoundXPos;
 			}
-			
-			//Reset the draw location for the images
-			this.getBound().x = BoundXPos;
-			
+		} catch (Exception e) {
+			// Log.e("Text Error", "BitmapFont draw error : " + e);
 		}
 	}
 
