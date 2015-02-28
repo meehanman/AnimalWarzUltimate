118e565eaac2a1c52e8f9854f713008f91609e69
40130351 <40130351@755a74bb-203b-7d4a-9881-852534eabff6>
2015-02-28 16:46:25 +0000

    MJ - redone team management, everything works swimmingly now :)

--- AnimalWarzUltimate/src/com/threeml/awu/world/gameobject/player/Team.java
+++ AnimalWarzUltimate/src/com/threeml/awu/world/gameobject/player/Team.java
@@ -2,134 +2,105 @@ package com.threeml.awu.world.gameobject.player;
 
 import java.util.List;
 
+import android.util.Log;
+
 /**
- * Team object is used to hold all the players in a team
- * Use the team ID to distinguish between teams and set if team is currently
- * active using Active variable
+ * Team object is used to hold all the players in a team Use the team ID to
+ * distinguish between teams and set if team is currently active using Active
+ * variable
  * 
  * @author Mary-Jane
- *
+ * 
  */
 public class Team {
 	// /////////////////////////////////////////////////////////////////////////
 	// Attributes
 	// /////////////////////////////////////////////////////////////////////////
-	
+
 	/** List of players in this team */
 	private List<Player> mPlayers;
 	/** Team name that is displayed */
 	private String mTeamName;
-	/** If active is true then it is the team the user interacts with */
-	private boolean mActive = false;
-	/** Team ID used to distinguish between teams */
-	private int mId;
-	/** The last active Player's ID */
-	private int mActivePlayerId;
-	
+	/** Last player active in the team */
+	private Player mLastActivePlayer;
+
 	// /////////////////////////////////////////////////////////////////////////
 	// Constructors
 	// /////////////////////////////////////////////////////////////////////////
-	
+
 	/**
 	 * Creates a new Team object for managing players
 	 * 
 	 * @param players
-	 * 				List of players in this team
+	 *            List of players in this team
 	 * @param teamName
-	 * 				The team name that is displayed
+	 *            The team name that is displayed
 	 */
-	public Team(List<Player> players, String teamName){
+	public Team(List<Player> players, String teamName) {
 		mPlayers = players;
 		mTeamName = teamName;
 	}
-	
+
 	// /////////////////////////////////////////////////////////////////////////
 	// Methods
 	// /////////////////////////////////////////////////////////////////////////
-	
-	public void nextActivePlayer(){
-		if(mPlayers.indexOf(getActivePlayer()) < mPlayers.size()){
-			setActivePlayerByIndex(mPlayers.indexOf(getActivePlayer()) + 1);
-		}else {
-			setActivePlayerByIndex(0);
-		}
-	}
-	
+
 	/**
 	 * Returns the value of the all health of all players in the team
 	 * 
 	 * @return collectiveHealth
 	 */
-	public int getCollectiveHealth(){
+	public int getCollectiveHealth() {
 		int collectiveHealth = 0;
-		for(Player p : mPlayers){
+		for (Player p : mPlayers) {
 			collectiveHealth += p.getHealth();
 		}
 		return collectiveHealth;
 	}
-	
+
 	/**
-	 * Set the active player using the player's ID
+	 * Change the active player in the team
 	 * 
-	 * @param id
+	 * @return active player
 	 */
-	public void setActivePlayerById(int id){
-		for(Player p : mPlayers){
-			if(p.getId() == id){
-				p.setActive(true);
-				mActivePlayerId = p.getId();
-			} else{
-				p.setActive(false);
+	public Player changeActivePlayer() {
+		if (mLastActivePlayer == null) {
+			mLastActivePlayer = mPlayers.get(0);
+		} else {
+			try {
+				if (mPlayers.indexOf(mLastActivePlayer) < (mPlayers.size() - 1)) {
+					mLastActivePlayer = mPlayers.get(mPlayers
+							.indexOf(mLastActivePlayer) + 1);
+				} else {
+					mLastActivePlayer = mPlayers.get(0);
+				}
+			} catch (Exception e) {
+				Log.e("TeamError", "Error in Team changeActivePlayer : "
+						+ e);
 			}
 		}
+		return mLastActivePlayer;
 	}
 	
-	public void addNewPlayer(Player p){
+	/**
+	 * Adds a new player to the team
+	 * 
+	 * @param p
+	 * 			Player
+	 */
+	public void addNewPlayer(Player p) {
 		mPlayers.add(p);
 	}
-	
-	/**
-	 * Set the active player by it's index in the player's list
-	 * 
-	 * @param index
-	 */
-	public void setActivePlayerByIndex(int index){
-		for(Player p : mPlayers){
-			p.setActive(false);
-		}
-		mPlayers.get(index).setActive(true);
-	}
-	
-	/**
-	 * Get the active player in this team
-	 * 
-	 * @return player
-	 */
-	public Player getActivePlayer(){
-		Player ActivePlayer = null;
-		
-		for(Player p : mPlayers){
-			if(p.getActive()){
-				ActivePlayer = p;
-			}
-		}
-		//We can never return null here,
-		//or the game WILL crash on update
-		if(ActivePlayer == null){
-			//Set An active player to the first player?
-			ActivePlayer = mPlayers.get(0);
-		}
-		return ActivePlayer;
-	}
+
 	/**
 	 * Gets the size of the list of players
 	 * 
 	 * @return teamSize
 	 */
-	public int getTeamSize(){
+	public int size() {
 		return mPlayers.size();
 	}
-	
+
 	/**
 	 * Get the list of players in this team
 	 * 
@@ -138,6 +109,7 @@ public class Team {
 	public List<Player> getPlayers() {
 		return mPlayers;
 	}
+
 	/**
 	 * Set the list of players for this team
 	 * 
@@ -146,54 +118,23 @@ public class Team {
 	public void setPlayers(List<Player> players) {
 		mPlayers.addAll(players);
 	}
+
 	/**
-	 * Get the team name 
+	 * Get the team name
 	 * 
 	 * @return teamName
 	 */
 	public String getTeamName() {
 		return mTeamName;
 	}
+
 	/**
 	 * Set the team name
 	 * 
 	 * @param teamName
 	 */
-	public void setTeamName(String teamName) {
+	public void setName(String teamName) {
 		mTeamName = teamName;
 	}
-	/**
-	 * Gets if the team is currently active
-	 * 
-	 * @return active
-	 */
-	public boolean isActive() {
-		return mActive;
-	}
-	/**
-	 * Set if team is currently active
-	 * 
-	 * @param active
-	 */
-	public void setActive(boolean active) {
-		mActive = active;
-	}
-	/**
-	 * Get the team ID
-	 * 
-	 * @return id
-	 */
-	public int getId() {
-		return mId;
-	}
-	/**
-	 * Set the team ID
-	 * 
-	 * @param id
-	 */
-	public void setId(int id) {
-		mId = id;
-	}
-	
-	
+
 }
