96ad74435edc49afc2bd82ed9c99fd5766216535
40130351 <40130351@755a74bb-203b-7d4a-9881-852534eabff6>
2015-03-01 22:11:06 +0000

    MJ - By popular demand, I have added comments!

--- AnimalWarzUltimate/src/com/threeml/awu/world/gameobject/GameObjectText.java
+++ AnimalWarzUltimate/src/com/threeml/awu/world/gameobject/GameObjectText.java
@@ -8,12 +8,40 @@ import com.threeml.awu.world.GameObject;
 import com.threeml.awu.world.GameScreen;
 import com.threeml.awu.world.LayerViewport;
 import com.threeml.awu.world.ScreenViewport;
-
+/**
+ * Used for text that is bound to a game object, acts as a label
+ * 
+ * @author Mary-Jane
+ *
+ */
 public class GameObjectText extends BitmapFont{
 	
+	// /////////////////////////////////////////////////////////////////////////
+	// Attributes
+	// /////////////////////////////////////////////////////////////////////////
+	
+	/** The game object that the text is bound to */
 	private GameObject mGameObj;
+	/** The distance from the center of the game object to display the text */
 	private int mHeightFromObject;
 	
+	
+	// /////////////////////////////////////////////////////////////////////////
+	// Constructors
+	// /////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Creates new game object text
+	 * 
+	 * @param gameScreen
+	 * 				Gamescreen to which text belongs
+	 * @param str
+	 * 				Text to display 
+	 * @param gameObj
+	 * 				Game object that the text is bound to
+	 * @param heightFromObject
+	 * 				Distance from the center of the game object to display the text
+	 */
 	public GameObjectText(GameScreen gameScreen, String str, GameObject gameObj, int heightFromObject){
 		super(gameObj.getBound().x, gameObj.getBound().y + heightFromObject, gameScreen, str);
 		
@@ -21,6 +49,14 @@ public class GameObjectText extends BitmapFont{
 		mHeightFromObject = heightFromObject;
 	}
 	
+	// /////////////////////////////////////////////////////////////////////////
+	// Methods
+	// /////////////////////////////////////////////////////////////////////////
+	/**
+	 * Update the text
+	 * @param elapsedTime
+	 *            	Elapsed time information
+	 */
 	public void update(ElapsedTime elapsedTime) {
 		
 		//TODO - this is awful hacked together, need a better way to align the center of text to center of player
@@ -32,6 +68,18 @@ public class GameObjectText extends BitmapFont{
 
 	}
 	
+	/**
+	 * Draw method to draw the text object to the screen
+	 * 
+	 * @param elapsedTime
+	 *            Elapsed time information
+	 * @param graphics2D
+	 *            Graphics instance
+	 * @param layerViewport
+	 *            Game layer viewport
+	 * @param screenViewport
+	 *            Screen viewport
+	 */
 	public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D,
 			LayerViewport layerViewport, ScreenViewport screenViewport) {
 
