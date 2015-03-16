af63240fba54dbd88700636b8906b67390996fc3
40130351 <40130351@755a74bb-203b-7d4a-9881-852534eabff6>
2015-03-16 16:46:34 +0000

    MJ - updated weapon shooting/change active player

--- AnimalWarzUltimate/src/com/threeml/awu/game/SmallMap.java
+++ AnimalWarzUltimate/src/com/threeml/awu/game/SmallMap.java
@@ -2,13 +2,18 @@ package com.threeml.awu.game;
 
 import android.graphics.Bitmap;
 import android.graphics.Rect;
-
+/**
+ * Class created to handle the map selection in the team selection menu
+ * 
+ * @author Mary-Jane
+ *
+ */
 public class SmallMap {
+	/** Holds the small map image */
 	public Bitmap mapImage;
+	/** Holds the map name */
 	public String mapName;
+	/** Holds the bound for the button */
 	public Rect mapBound;
 
-	public SmallMap() {
-
-	}
 }
